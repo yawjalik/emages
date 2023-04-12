4718e328fd6abb144e63e4097138ec5ed8d17f0d
Jalik Yaw <70761798+yawjalik@users.noreply.github.com>
2023-04-13 01:38:42 +0800

    Added missing pages (#6)
    * Added missing pages
    
    * Added chakra icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,31 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Box, Button, ChakraProvider, HStack, Heading, extendTheme } from '@chakra-ui/react';
+import Link from 'next/link';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <Head>
+        <title>EMages</title>
+        <meta name="description" content="Images" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box bg="teal.500" w="100%" h="4px" />
+      <Box margin={4}>
+        <HStack spacing={8} justify="around" marginBottom={8}>
+          <Heading size="xl">EMages</Heading>
+          <Link href="/"><Button colorScheme="teal" variant="ghost">Search</Button></Link>
+          <Link href="/upload"><Button colorScheme="teal" variant="ghost">Upload</Button></Link>
+        </HStack>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  </QueryClientProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
+import { FC, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import ImageGallery from "@/components/ImageGallery";
+import { useToast, Spinner, Heading, VStack, ScaleFade } from "@chakra-ui/react";
+import SearchBar from "@/components/SearchBar";
+
+const SearchImages: FC = () => {
+  const [search, setSearch] = useState<string>("");
+  const router = useRouter();
+  const toast = useToast();
+
+  const { data, isLoading, isFetching, refetch } = useQuery("images", async () => {
+    const { q } = router.query;
+    const res = await fetch(`/api/search${q ? "?q=" + q : ""}`, { method: "GET" });
+    if (!res.ok) {
+      const { message } = await res.json();
+      toast({ title: message, status: "error", duration: 3000, isClosable: true });
+      return;
+    }
+    const images = await res.json();
+    return images;
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [router.query, refetch]);
+
   return (
-    <>
-      <h1>Init!</h1>
-    </>
-  )
+      <VStack spacing={10}>
+        <SearchBar search={search} setSearch={setSearch} isLoading={isLoading} />
+        {
+          isLoading || isFetching ? <Spinner color="teal.500" emptyColor="gray.200" size="xl" thickness="4px" speed="0.65s"/> : 
+          data ? 
+          <ScaleFade in={true}>
+            <ImageGallery images={data} /> 
+          </ScaleFade>
+          : <Heading size="lg">No images found</Heading>
+        }
+      </VStack>
+  );
 }
+
+export default SearchImages;
